e792daf61b94e09cce657802a03d99fd42c41062
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2017-05-06 20:38:10 +0200

    Refactorizado algunos métodos. FizzBuzz function has to return \"12Fizz4Buzz\" with 5 as parameter

--- Jasmine/spec/FizzBuzzSpec.js
+++ Jasmine/spec/FizzBuzzSpec.js
@@ -1,18 +1,30 @@
-describe("FizzBuzz function", function () {
+describe("Decompose function", function () {
 	it("has to return \"Fizz\" with 3 as parameter", function () {
-		expect(fizzBuzz(3)).toBe("Fizz");
+		expect(decompose(3)).toBe("Fizz");
 	});
 });
 
 
-describe("FizzBuzz function", function () {
+describe("Decompose function", function () {
 	it("has to return \"Buzz\" with 5 as parameter", function () {
-		expect(fizzBuzz(5)).toBe("Buzz");
+		expect(decompose(5)).toBe("Buzz");
+	});
+});
+
+describe("Decompose function", function () {
+	it("has to return \"FizzBuzz\" with 15 as parameter", function () {
+		expect(decompose(15)).toBe("FizzBuzz");
+	});
+});
+
+describe("Decompose function", function () {
+	it("has to return \"1\" with 1 as parameter", function () {
+		expect(decompose(1)).toBe("1");
 	});
 });
 
 describe("FizzBuzz function", function () {
-	it("has to return \"FizzBuzz\" with 15 as parameter", function () {
-		expect(fizzBuzz(15)).toBe("FizzBuzz");
+	it("has to return \"12Fizz4Buzz\" with 5 as parameter", function () {
+		expect(fizzBuzz(5)).toBe("12Fizz4Buzz");
 	});
 });
--- Jasmine/src/FizzBuzz.js
+++ Jasmine/src/FizzBuzz.js
@@ -1,9 +1,21 @@
-function fizzBuzz(string) {
+function decompose(string) {
 	if (string % 3 == 0 && string % 5 == 0) {
 		return "FizzBuzz";
 	} else if (string % 3 == 0) {
 		return "Fizz";
 	} else if (string % 5 == 0) {
 		return "Buzz";
+	} else {
+		return String(string);
 	}
+}
+
+function fizzBuzz(limit) {
+	var fizzBuzzString = "";
+
+	for (var value = 1; value <= limit; value++) {
+		fizzBuzzString += decompose(value);
+	}
+
+	return fizzBuzzString;
 }
