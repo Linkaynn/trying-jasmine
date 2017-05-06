eeea6802bdafa8ace43143cde62c3b8e3bfd957b
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2017-05-06 20:21:59 +0200

    FizzBuzz function has to return "Buzz" as 5 as parameter.

--- Jasmine/spec/FizzBuzzSpec.js
+++ Jasmine/spec/FizzBuzzSpec.js
@@ -1,5 +1,12 @@
 describe("FizzBuzz function", function () {
 	it("has to return \"Fizz\" with 3 as parameter", function () {
-		expect(fizzbuzz(3)).toBe("Fizz");
+		expect(fizzBuzz(3)).toBe("Fizz");
+	});
+});
+
+
+describe("FizzBuzz function", function () {
+	it("has to return \"Buzz\" with 5 as parameter", function () {
+		expect(fizzBuzz(5)).toBe("Buzz");
 	});
 });
--- Jasmine/src/FizzBuzz.js
+++ Jasmine/src/FizzBuzz.js
@@ -1,5 +1,7 @@
-function fizzbuzz(string) {
+function fizzBuzz(string) {
 	if (string % 3 == 0) {
 		return "Fizz";
+	} else if (string % 5 == 0) {
+		return "Buzz";
 	}
 }
