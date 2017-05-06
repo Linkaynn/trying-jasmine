affc8db36c361946d42f50a23748040f93d2def2
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2017-05-06 20:42:43 +0200

    Test refactorizados

--- Jasmine/spec/FizzBuzzSpec.js
+++ Jasmine/spec/FizzBuzzSpec.js
@@ -1,36 +1,29 @@
 describe("Decompose function", function () {
-	it("has to return \"Fizz\" with 3 as parameter", function () {
+
+	it("Has to return \"Fizz\" with 3 as parameter", function () {
 		expect(translate(3)).toBe("Fizz");
 	});
-});
 
-
-describe("Decompose function", function () {
-	it("has to return \"Buzz\" with 5 as parameter", function () {
+	it("Has to return \"Buzz\" with 5 as parameter", function () {
 		expect(translate(5)).toBe("Buzz");
 	});
-});
 
-describe("Decompose function", function () {
-	it("has to return \"FizzBuzz\" with 15 as parameter", function () {
+	it("Has to return \"FizzBuzz\" with 15 as parameter", function () {
 		expect(translate(15)).toBe("FizzBuzz");
 	});
-});
 
-describe("Decompose function", function () {
-	it("has to return \"1\" with 1 as parameter", function () {
+	it("Has to return \"1\" with 1 as parameter", function () {
 		expect(translate(1)).toBe("1");
 	});
+
 });
 
 describe("FizzBuzz function", function () {
-	it("has to return \"12Fizz4Buzz\" with 5 as parameter", function () {
+	it("Has to return \"12Fizz4Buzz\" with 5 as parameter", function () {
 		expect(fizzBuzz(5)).toBe("12Fizz4Buzz");
 	});
-});
 
-describe("FizzBuzz function", function () {
-	it("has to return \"12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz\" with 15 as parameter", function () {
+	it("Has to return \"12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz\" with 15 as parameter", function () {
 		expect(fizzBuzz(15)).toBe("12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz");
 	});
 });
